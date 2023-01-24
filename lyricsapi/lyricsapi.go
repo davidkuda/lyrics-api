ef042563ba55313c415f7e3cc6462308bab16455
davidkuda <david@kuda.ai>
2023-01-24 21:10:30 +0100

    feat: define struct Song

--- lyricsapi/lyricsapi.go
+++ lyricsapi/lyricsapi.go
@@ -4,6 +4,17 @@ import (
 	"net/http"
 )
 
+type Songs []Song
+
+type Song struct {
+	Artist    string
+	SongName  string
+	SongText  string
+	Chords    string
+	Copyright string
+	Covers    []string // list of URLs to great covers, e.g. on YouTube
+}
+
 // handler: get all songs
 func handleListSongs(w http.ResponseWriter, r *http.Request) {
 	// get list of songs from db io

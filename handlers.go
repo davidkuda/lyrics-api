a7145258208d659514665f497fbd455d075b4d96
davidkuda <david@kuda.ai>
2023-01-25 17:24:33 +0100

    update handlers.go

--- handlers.go
+++ handlers.go
@@ -1,10 +1,13 @@
-package lyricsapi
+package main
 
 import (
 	"net/http"
+
+	"github.com/davidkuda/lyricsapi/dbio"
 )
 
 // handler: get all songs
-func handleListSongs(w http.ResponseWriter, r *http.Request) {
+func HandleListSongs(w http.ResponseWriter, r *http.Request) {
 	// get list of songs from db io
+	songs := dbio.ListSongs()
 }

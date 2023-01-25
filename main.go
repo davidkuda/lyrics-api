e726901fc03f296868b7a37644690b5d9f27c3dc
davidkuda <david@kuda.ai>
2023-01-25 22:22:40 +0100

    feat: use json fields in Song

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 type Songs []Song
 
 type Song struct {
-	Artist    string
-	SongName  string
-	SongText  string
-	Chords    string
-	Copyright string
-	Covers    []string // list of URLs to great covers, e.g. on YouTube
+	Artist    string `json:"artist"`
+	SongName  string `json:"song_name"`
+	SongText  string `json:"song_text,omitempty"`
+	Chords    string `json:"chords,omitempty"`
+	Copyright string `json:"copyright,omitempty"`
+	// ? How to apply comments correctly?
+	// Covers: list of URLs to great covers, e.g. on YouTube
+	Covers []string `json:"covers,omitempty"`
 }
 
 func main() {

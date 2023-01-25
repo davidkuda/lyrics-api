484d23802a39bc3434084e3201cccee7cbd4ba86
davidkuda <david@kuda.ai>
2023-01-25 17:21:11 +0100

    refactor: define structs in main pkg

--- main.go
+++ main.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+type Songs []Song
+
+type Song struct {
+	Artist    string
+	SongName  string
+	SongText  string
+	Chords    string
+	Copyright string
+	Covers    []string // list of URLs to great covers, e.g. on YouTube
+}
+
 func main() {
 	fmt.Println("Hello World")
 }

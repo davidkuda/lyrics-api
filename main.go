d8b52f1bdf16ca19e490d000e98b88f83f34b8b3
davidkuda <david@kuda.ai>
2023-01-26 17:21:54 +0100

    docs: add documentation to fields of Song

--- main.go
+++ main.go
@@ -7,13 +7,20 @@ import (
 
 type Songs []Song
 
+// Song contains all data related to a piece of music
 type Song struct {
-	Artist    string `json:"artist"`
-	SongName  string `json:"song_name"`
-	SongText  string `json:"song_text,omitempty"`
-	Chords    string `json:"chords,omitempty"`
+	// slug of the song, song name with hyphens, e.g. "wish-you-were-here"
+	SongID string `json:"song_id"`
+	// artist of the song, e.g. "Pink Floyd"
+	Artist string `json:"artist"`
+	// name of the song
+	SongName string `json:"song_name"`
+	// lyrics, text of the song
+	SongText string `json:"song_text,omitempty"`
+	// chords of the song, plain text
+	Chords string `json:"chords,omitempty"`
+	// copyright information of the song
 	Copyright string `json:"copyright,omitempty"`
-	// ? How to apply comments correctly?
 	// Covers: list of URLs to great covers, e.g. on YouTube
 	Covers []string `json:"covers,omitempty"`
 }

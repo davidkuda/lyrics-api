c08b27bb15cae4de3e23c272a0e29aa7303d53a5
davidkuda <david@kuda.ai>
2023-02-06 23:52:13 +0100

    feat: log requests on the server

--- handlers.go
+++ handlers.go
@@ -7,11 +7,35 @@ import (
 	"strings"
 )
 
+// ? how can you write logs to a file? can you write to stdout and to a file? (i.e. to multiple files?)
+type requestLog struct {
+	URL      string `json:"url"`
+	Method   string `json:"method"`
+	BodySize int64  `json:"content_length"`
+	Protocol string `json:"protocol"`
+}
+
+func logRequest(r *http.Request, cfg *appConfig) {
+	l := requestLog{
+		URL:      r.URL.String(),
+		Method:   r.Method,
+		BodySize: r.ContentLength,
+		Protocol: r.Proto,
+	}
+
+	j, err := json.Marshal(&l)
+	if err != nil {
+		panic(err)
+	}
+	cfg.logger.Println(string(j))
+}
+
 func setupHandlers(mux *http.ServeMux, config appConfig) {
 	mux.Handle("/songs/", &app{config: config, handler: handleSongs})
 }
 
 func handleSongs(w http.ResponseWriter, r *http.Request, cfg appConfig) {
+	logRequest(r, &cfg)
 	if len(r.URL.Path) > len("/songs/") {
 		id := strings.TrimPrefix(r.URL.Path, "/songs/")
 		returnSong(w, r, id, &cfg)

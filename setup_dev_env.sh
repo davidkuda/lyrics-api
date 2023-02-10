365683282ab8173a0bf9740d04a2216f7089da79
davidkuda <david@kuda.ai>
2023-02-10 22:00:36 +0100

    commit script that sets env vars

---
+++ setup_dev_env.sh
@@ -0,0 +1,11 @@
+# run "source ./setup_test_env.sh" to export env var for the session
+export DB_ADDR="localhost:5432"
+export DB_NAME="lyricsapi"
+export DB_USER="lyricsapi"
+export DB_PASSWORD="lyricsapi"
+
+# jwt
+export JWT_SECRET="verysecret"
+export JWT_ISSUER="kuda.ai"
+export JWT_AUDIENCE="kuda.ai"
+export COOKIE_DOMAIN="localhost"

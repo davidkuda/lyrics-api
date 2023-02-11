365832a7132e8fd4123010524fcb7c50298d88d9
davidkuda <david@kuda.ai>
2023-02-11 22:07:29 +0100

    refactor: setup routes in routes.go

--- handlers.go
+++ handlers.go
@@ -30,11 +30,6 @@ func logRequest(r *http.Request, cfg *appConfig) {
 	cfg.logger.Println(string(j))
 }
 
-func setupHandlers(mux *http.ServeMux, app application) {
-	mux.HandleFunc("/songs/", app.handleSongs)
-	mux.HandleFunc("/healthz/", app.HealthCheckHandler)
-}
-
 func (a *application) HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Health is ok"))

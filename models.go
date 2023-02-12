b1830a50654e2208eba6b09e5ca788b6d24e8022
davidkuda <david@kuda.ai>
2023-02-12 23:43:07 +0100

    rfr: change datetime to time.Time

--- models.go
+++ models.go
@@ -1,11 +1,12 @@
 package main
 
 type User struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	EMail     string `json:"email"`
-	Password  string `json:"password"` // a hash of a password
-	CreatedAt string `json:"-"`        // a hyphen means it's not put into the json
-	UpdatedAt string `json:"-"`
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	EMail     string    `json:"email"`
+	Password  string    `json:"password"` // a hash of a password
+	CreatedAt time.Time `json:"-"`        // a hyphen means it's not put into the json
+	UpdatedAt time.Time `json:"-"`
+}
 }

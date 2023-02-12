27d9b1c8d6bd1f1cd95344d9b78b174289772a8c
davidkuda <david@kuda.ai>
2023-02-12 23:18:56 +0100

    feat: define repo pattern for db

--- dbio.go
+++ dbio.go
@@ -13,6 +13,14 @@ import (
 
 var ErrSongDoesNotExist = errors.New("Song does not exist")
 
+type DatabaseRepo interface {
+	Connection() *sql.DB
+	getDatabaseConn(dbAddr, dbName, dbUser, dbPassword string) (*sql.DB, error)
+	ListSongs(cfg appConfig) Songs
+	GetSong(songName string, cfg appConfig) (Song, error)
+	GetUserByEmail(email string, cfg appConfig) (*User, error)
+}
+
 // returns a pool of connections to the postgres db according to the args
 func getDatabaseConn(dbAddr, dbName, dbUser, dbPassword string) (*sql.DB, error) {
 	// "data source name": string of the url to the database

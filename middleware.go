048b038d298a7e8183eb73288ed341082991fb31
davidkuda <david@kuda.ai>
2023-02-18 21:30:09 +0100

    feat: create auth middleware

---
+++ middleware.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+
+func (app *application) authRequired(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _, err := app.auth.GetTokenFromHeaderAndVerify(w, r)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}

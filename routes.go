51e221d899e959124df54e85ab708e26a4891806
davidkuda <david@kuda.ai>
2023-02-28 20:20:35 +0100

    chore: rm comments

--- routes.go
+++ routes.go
@@ -6,11 +6,6 @@ import (
 	"github.com/davidkuda/lyricsapi/handlers"
 )
 
-// /v1/lyrics/ GET -> return a list of all available song names
-// /v1/lyrics/ POST -> create a new song
-// /v1/lyrics/{song-name} PATCH -> update an existing song
-// PATCH vs PUT: Patch for elements, PUT for replacing the whole row
-// /v1/lyrics/{song-name} GET -> return songtext with metadata
 func setupHandlers(mux *http.ServeMux, app handlers.Application) {
 	mux.HandleFunc("/songs", app.HandleSongs)
 	mux.HandleFunc("/healthz", app.HandleHealthCheck)

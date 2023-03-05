f32b46834632574e3ce44d5628ed41efd92a4d9a
davidkuda <david@kuda.ai>
2023-03-05 23:14:12 +0100

    feat: implement cors middleware

--- handlers/middleware.go
+++ handlers/middleware.go
@@ -14,3 +14,21 @@ func (app *Application) authRequired(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *Application) EnableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Origin")
+		origin := r.Header.Get("Origin")
+
+		if origin != "" {
+			for i := range app.Config.CORS.TrustedOrigins {
+				if origin == app.Config.CORS.TrustedOrigins[i] {
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+					break
+				}
+			}
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
--- main.go
+++ main.go
@@ -74,5 +74,5 @@ func main() {
 
 	log.Printf("Starting app; listening on port %s", listenAddr)
 	// ? ListenAndServe: If you terminate the process, the last requests may get lost. Check Ardan Labs "Service" to see an alternative.
-	log.Fatal(http.ListenAndServe(listenAddr, app.LogRequests(mux)))
+	log.Fatal(http.ListenAndServe(listenAddr, app.LogRequests(app.EnableCORS(mux))))
 }

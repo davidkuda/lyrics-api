1354c79d396b3e1365783d9a51d932dd722f1f8a
davidkuda <david@kuda.ai>
2023-03-07 22:31:46 +0100

    rfr: update helpers.go

--- handlers/helpers.go
+++ handlers/helpers.go
@@ -13,26 +13,26 @@ type JSONResponse struct {
 	Data    interface{} `json:"data",omitempty`
 }
 
+type envelope map[string]any
+
 // variadic parameter ... -> 0 or any
 func (app *Application) writeJSON(
-	w http.ResponseWriter, status int, data interface{}, headers ...http.Header,
+	w http.ResponseWriter, status int, data envelope, headers http.Header,
 ) error {
-	out, err := json.MarshalIndent(data, "", "  ")
+	js, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
-		return nil
+		return err
 	}
 
-	if len(headers) > 0 {
-		for _, header := range headers {
-			for key, value := range header {
-				w.Header()[key] = value
-			}
-		}
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	w.Write(out)
+	w.Write(js)
 
 	return nil
 }
@@ -68,5 +68,8 @@ func (app *Application) errorJSON(w http.ResponseWriter, err error, status ...in
 	payload.Error = true
 	payload.Message = err.Error()
 
-	return app.writeJSON(w, statusCode, payload)
+	env := envelope{"error": payload}
+	h := http.Header{}
+
+	return app.writeJSON(w, statusCode, env, h)
 }

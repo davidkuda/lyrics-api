9132b2ce0a1cf92aea2540ce5423f1619ad3f6fd
davidkuda <david@kuda.ai>
2023-03-09 21:44:47 +0100

    fix: remove / from end in route

--- routes.go
+++ routes.go
@@ -7,7 +7,7 @@ import (
 )
 
 func setupHandlers(mux *http.ServeMux, app handlers.Application) {
-	mux.HandleFunc("/songs", app.HandleSongs)
+	mux.HandleFunc("/songs/", app.HandleSongs)
 	mux.HandleFunc("/healthz", app.HandleHealthCheck)
 	mux.HandleFunc("/signup", app.Signup)
 	mux.HandleFunc("/signin", app.Signin)

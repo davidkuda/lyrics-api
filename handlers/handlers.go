a7407e1283b36ba39249934c2994ab4224f7b385
davidkuda <david@kuda.ai>
2023-03-11 00:21:51 +0100

    rfr: remove jwt stuff from application

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,21 +3,12 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/davidkuda/lyricsapi/auth"
 	"github.com/davidkuda/lyricsapi/config"
 )
 
 type Application struct {
 	Config  config.AppConfig
 	Handler func(w http.ResponseWriter, r *http.Request, config config.AppConfig)
-
-	Domain string
-
-	Auth         auth.Auth
-	JWTSecret    string
-	JWTIssuer    string
-	JWTAudience  string
-	CookieDomain string
 }
 
 func (app Application) ServeHTTP(w http.ResponseWriter, r *http.Request) {

f078bbc2d4622a307a92ecb64a9c8bd908d39c22
davidkuda <david@kuda.ai>
2023-03-11 00:47:40 +0100

    rfr: have a handler for /songs and /songs/

--- routes.go
+++ routes.go
@@ -6,8 +6,14 @@ import (
 	"github.com/davidkuda/lyricsapi/handlers"
 )
 
+// Go’s servemux supports two different types of URL patterns:
+// fixed paths and subtree paths. Fixed paths do not end with a
+// trailing slash, whereas subtree paths do end with a trailing
+// slash.
+
 func setupHandlers(mux *http.ServeMux, app handlers.Application) {
-	mux.HandleFunc("/songs/", app.HandleSongs)
+	mux.HandleFunc("/songs", app.HandleSongsFixedPath)
+	mux.HandleFunc("/songs/", app.HandleSongsSubtreePath)
 	mux.HandleFunc("/healthz", app.HandleHealthCheck)
 	mux.HandleFunc("/signup", app.Signup)
 	mux.HandleFunc("/v1/tokens/authentication", app.CreateAuthenticationTokenHandler)

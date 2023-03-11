6c8edd0c7e3252df65a1282dafc07029c8188ba9
davidkuda <david@kuda.ai>
2023-03-11 01:01:10 +0100

    chore: rm jwt env vars

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -4,11 +4,5 @@ export DB_NAME="lyricsapi"
 export DB_USER="lyricsapi"
 export DB_PASSWORD="lyricsapi"
 
-# jwt
-export JWT_SECRET="verysecret"
-export JWT_ISSUER="kuda.ai"
-export JWT_AUDIENCE="kuda.ai"
-export COOKIE_DOMAIN="localhost"
-
 # CORS
 export ALLOWED_CORS_ORIGINS="http://localhost:3000 http://localhost:8008 http://localhost:3001"

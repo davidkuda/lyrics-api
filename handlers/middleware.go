f327c89666b2f8a8638483474eb17e04a4be5136
davidkuda <david@kuda.ai>
2023-03-15 23:47:33 +0100

    chore: rm unwanted fmt.Println

--- handlers/middleware.go
+++ handlers/middleware.go
@@ -60,7 +60,6 @@ func (app *Application) EnableCORS(next http.Handler) http.Handler {
 		if origin != "" {
 			for i := range app.Config.CORS.TrustedOrigins {
 				if origin == app.Config.CORS.TrustedOrigins[i] {
-					fmt.Println(origin)
 					w.Header().Set("Access-Control-Allow-Origin", origin)
 					w.Header().Set("Access-Control-Allow-Credentials", "true")
 					w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")

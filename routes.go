1e586d10dc717d958aadc7112ead58daa9a43adc
davidkuda <david@kuda.ai>
2023-03-18 09:32:34 +0100

    remove old route SignUp

--- routes.go
+++ routes.go
@@ -15,6 +15,5 @@ func setupHandlers(mux *http.ServeMux, app handlers.Application) {
 	mux.HandleFunc("/healthz", app.HandleHealthCheck)
 	mux.HandleFunc("/songs", app.HandleSongsFixedPath)
 	mux.HandleFunc("/songs/", app.HandleSongsSubtreePath)
-	// mux.HandleFunc("/signup", app.Signup)
 	mux.HandleFunc("/v1/tokens/authentication", app.CreateAuthenticationTokenHandler)
 }

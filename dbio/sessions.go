83888c36c79e705b6c55322978ffcdcff9232f31
davidkuda <david@kuda.ai>
2023-03-19 22:57:37 +0100

    feat: delete expired tokens

--- dbio/sessions.go
+++ dbio/sessions.go
@@ -71,3 +71,24 @@ func GetSessionToken(token string, db *sql.DB) (models.SessionToken, error) {
 
 	return t, nil
 }
+
+func DeleteExpiredTokens(db *sql.DB) error {
+	ctx := context.Background()
+	conn, err := db.Conn(ctx)
+	if err != nil {
+		return fmt.Errorf("sql.Open: %v\n", err)
+	}
+	defer conn.Close()
+
+	query := `
+		DELETE FROM sessions
+		WHERE expiry < NOW();
+	`
+
+	_, err = conn.ExecContext(ctx, query)
+	if err != nil {
+		return fmt.Errorf("conn.ExecContext: %v\n", err)
+	}
+	
+	return nil
+}

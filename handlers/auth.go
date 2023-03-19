e969faf32753493fe6cfb95095d1e4305820a221
davidkuda <david@kuda.ai>
2023-03-19 22:28:49 +0100

    make session cookie httponly and secure

--- handlers/auth.go
+++ handlers/auth.go
@@ -68,9 +68,11 @@ func (app *Application) Authenticate(w http.ResponseWriter, r *http.Request) {
 
 	// send session token as cookie
 	http.SetCookie(w, &http.Cookie{
-		Name:    "session",
-		Value:   t.Token,
-		Expires: t.Expiry,
+		Name:     "session",
+		Value:    t.Token,
+		Expires:  t.Expiry,
+		Secure:   true,
+		HttpOnly: true,
 	})
 
 	w.WriteHeader(http.StatusCreated)

6f9817c07e964f27318dc56ecdcf6b3bc8d97e01
davidkuda <david@kuda.ai>
2023-04-08 22:58:34 +0200

    use Domain "lyricsapi.kuda.ai"

--- handlers/auth.go
+++ handlers/auth.go
@@ -71,7 +71,7 @@ func (app *Application) Authenticate(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, &http.Cookie{
 		Name:     "session",
 		Value:    t.Token,
-		Domain:   "lyricsapi.onrender.com",
+		Domain:   "lyricsapi.kuda.ai",
 		Expires:  t.Expiry,
 		Secure:   true,
 		HttpOnly: true,
